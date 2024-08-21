0df7be0cd0e7e0a633d9975bc461900a61959fe1
gamalabdu <abdu.s.gamal@gmail.com>
2024-08-21 09:16:35 -0400

    adding changes to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import beverlyHills from '../assets/beverlyHills.mp4'
 import lawLogo3 from '../assets/lawLogo3.png'
 import './styles.css'
+import { FaLocationDot } from "react-icons/fa6";
 
 const Home = () => {
 
@@ -32,9 +33,9 @@ const Home = () => {
                 With Immigration Law Concerns
         </h2>
 
-        <div className='flex gap-16 text-lg font-semibold'>
-          <div> Beverly Hills, CA </div>
-          <div> La Jolla, San Diego County </div>
+        <div className='flex flex-col items-center gap-4 text-center text-base font-semibold'>
+              <div className='flex align-middle items-center gap-2'> <FaLocationDot /> Beverly Hills, CA </div>
+              <div className='flex align-middle items-center gap-2'> <FaLocationDot /> La Jolla, San Diego County </div>
         </div>
 
         <button onClick={ () => navigate('/about') } className='bg-blue-950 text-neutral-100 pt-2 pb-2 pl-4 pr-4 rounded-md font-bold hover:bg-slate-800'> Learn More </button>
